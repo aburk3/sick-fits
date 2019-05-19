2d7ec385dac4d03ba8d34406a12ec618908813f6
aburk3 <aburk3@gmail.com>
2019-05-19 13:53:20 -0500

    Adding nav item links with appropriate styling

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f28311f87326f1eeef37";
+/******/ 	var hotCurrentHash = "81742f2adf02b58a2298";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
